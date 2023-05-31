77d97af23c9c5a63c53a2302719a53bb34d99a91
Niraj <neeraz.pandey2058@gmail.com>
2023-05-31 22:06:54 +0545

    Made a tracker page and changed models and others.

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from .models import Product, Contact, Order
+from .models import Product, Contact, Order, OrderUpdate
 from math import ceil
+import json
 
 # Create your views here.
 from django.http import HttpResponse
+
 
 def index(request):
     allProds = []
     catProds = Product.objects.values('category')
     cats = {item['category'] for item in catProds}
     for cat in cats:
-        prod = Product.objects.filter(category = cat)
+        prod = Product.objects.filter(category=cat)
         n = len(prod)
         nSlides = n//4 + ceil((n/4)-(n//4))
         allProds.append([prod, range(1, nSlides), nSlides])
@@ -18,8 +20,10 @@ def index(request):
     params = {'allProds': allProds}
     return render(request, 'shop/index.html', params)
 
+
 def about(request):
     return render(request, 'shop/about.html')
+
 
 def contact(request):
     if request.method == "POST":
@@ -29,19 +33,41 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-    
+
     return render(request, 'shop/contact.html')
 
+
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    if request.method == "POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Order.objects.filter(order_id=orderId, email=email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append(
+                        {'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
+    return render(request, 'shop/tracker.html')
+
 
 def search(request):
     return HttpResponse("We are at search")
 
+
 def prodView(request, myid):
 
-    product = Product.objects.filter(id = myid)
-    return render(request, 'shop/productview.html', {'product' : product[0]})
+    product = Product.objects.filter(id=myid)
+    return render(request, 'shop/productview.html', {'product': product[0]})
+
 
 def checkout(request):
     if request.method == "POST":
@@ -54,12 +80,16 @@ def checkout(request):
         state = request.POST.get('state', '')
         zip = request.POST.get('zip', '')
         phone = request.POST.get('phone', '')
-        orders = Order(itemsJson = itemsJson , name=name, email=email, address=address, address_2=address_2, city=city, state=state, zip=zip, phone=phone)
+        orders = Order(itemsJson=itemsJson, name=name, email=email, address=address,
+                       address_2=address_2, city=city, state=state, zip=zip, phone=phone)
         orders.save()
+
+        update = OrderUpdate(order_id=orders.order_id,
+                             update_desc='Your order has been placed!')
+        update.save()
 
         done = True
         id = Order.order_id
-        return render(request, 'shop/checkout.html', {'done' : done, 'id' : id})
+        return render(request, 'shop/checkout.html', {'done': done, 'id': id})
 
     return render(request, 'shop/checkout.html')
-
